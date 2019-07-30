8f92d4bd10ea5379d4a074851db6fcc85dd06b3e
RichardPony <1607700419@qq.com>
2019-07-30 17:24:41 +0800

    7/30/FirstPush

--- pages/PostDetail/detail.js
+++ pages/PostDetail/detail.js
@@ -1,5 +1,5 @@
+const app = getApp();
 Page({
-
   data: {
     rec_addr:null,
     time:null,
@@ -9,19 +9,32 @@ Page({
       { 'id': '3', 'name': '半 年' },
       { 'id': '4', 'name': '一 年' },
       { 'id': '5', 'name': '两 年' },
-      { 'id': '6', 'name': '自定义' },
+      // { 'id': '6', 'name': '自定义' },
     ],
-    btnId:'1'
+    userDefdate: null,
+    btnId: null,
+    isuserDef: false
+  },
+  
+  onReady() {
   },
 
-  onLoad: function (options) {
-    
-  },
-  timeClick: function(e){
-    var btnId = e.currentTarge.dataset.id;
+  bindDateChange: function(e){
+    console.log("时间：" + e.detail.value);
     this.setData({
-      btnId: btnId
+      btnId: '0',
+      userDefdate: e.detail.value,
+      isuserDef: true
     })
+  },
+
+  timeClick: function(e){
+    var btnId = e.currentTarget.dataset.id;
+    this.setData({
+      btnId: btnId,
+      isuserDef: false
+    })
+    console.log("按钮：" + this.data.btnId);
   },
   addr_input: function(e){
     this.setData({
